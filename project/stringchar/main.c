a9f02762d7fb560015a8a9b9eea8868af05d88f1
sutan mufti <upsutan@gmail.com>
2023-05-13 22:04:30 +0100

    removing the functions to the new c files

--- project/stringchar/main.c
+++ project/stringchar/main.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
-int strlenz(char *s)
- {
-        int n;
-        for (n = 0; *s != '\0'; s++)
-            n++;
-        
-        return n;
- } ;
-
-void printstring( char * s){
-    while (*s != '\0'){
-        printf("%c", *s);
-        s++;
-    }
-
-}
+#include "func.h"
 int main(){
 
     char a = 'a'; // use single quote to denote a char
